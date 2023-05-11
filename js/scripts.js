569adffe605810f36b08bc8341c813259944a46c
Mike Roberts <mroberts@endicott.edu>
2023-05-11 14:29:55 -0400

    finally fixed transform/transition/display issue
    Finally fixed the "mobile-menu sliding off the screen but still being visible if you scroll" issue. I added a timeout that toggles an "active" class on or off on the navmenu element. I then use the "active" class to set the display property to "block" if it is active.
    
    I also added a setTimeout so the display can be turned on before the transform/transition properties kick in, allowing the menu to "slide" onto the screen. I also had to set it up to work in the other direction as well. The menu "slides" off the screen, then after the sliding has completed (after a 300 millisecond timeout), the "active" class is toggled off, which causes the css to revert back to display: none.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,60 @@
 const navigationMenu = document.getElementById("navmenu");
 const navigationButton = document.getElementById("toggle-menu");
 
+
 function toggleMenu() {
     let isVisible = navigationMenu.getAttribute("data-visible");
-    console.log(isVisible);
+
     if(isVisible === "false") {
-        navigationMenu.setAttribute("data-visible", "true");
-        navigationButton.setAttribute("data-visible", "true");
+        // must first check to see if active is on. If not, call dataDisplay to turn on active, 
+        // then call this toggleMenu function again, but after a 300ms timeout.
+        // the timeout is necessary to allow the transform & transition properties to work correctly.
+        if(navigationMenu.classList.contains("active")) {
+            navigationMenu.setAttribute("data-visible", "true");
+            navigationButton.setAttribute("data-visible", "true");
+        }
+        else {
+            dataDisplay();
+            setTimeout(toggleMenu, 100);
+        }
     }
+    // if isVisible is true, then set both data-visible attributes to false, then 
+    // call dataDisplay to turn off the active class after a 300ms delay.
+    // the delay allows the css to slide the menu out the bottom of the screen
+    // before the menu display property is set to none. 
+    // without the delay, the transform & transition properties don't work.
     else {
         navigationMenu.setAttribute("data-visible", "false");
         navigationButton.setAttribute("data-visible", "false");
+        setTimeout(dataDisplay, 300);
+    }
+
+    // toggles the "active" class on and off for the navmenu. If active,
+    // display is set to block in the css. If off, display reverts
+    // back to none, which is the value set in the media query.
+    // It is necessary to set the display to none so the menu can't be seen
+    // when the user scrolls down.
+    function dataDisplay() {
+        navigationMenu.classList.toggle("active");
     }
 }
 
-function closeMobile() {
-    navigationMenu.setAttribute("data-visible", "false");
-    navigationButton.setAttribute("data-visible", "false");
 
-}
+
+// function closeMobile() {
+//     if(window.outerWidth > 767) {
+//         navigationMenu.setAttribute("data-visible", "true");
+//         navigationButton.setAttribute("data-visible", "true");
+//         setTimeout(dataDisplay("true"), 300);
+//     }
+//     else {
+//         navigationMenu.setAttribute("data-visible", "false");
+//         navigationButton.setAttribute("data-visible", "false");
+//         setTimeout(dataDisplay("false"), 300);
+        
+//     }
+
+// }
 
 navigationButton.addEventListener("click", toggleMenu);
 window.addEventListener("resize", closeMobile);
